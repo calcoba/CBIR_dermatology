1628238811891ce4114b2c0fcddb0209926b6fe9
carlos <calcobanunez@gmail.com>
2022-03-14 19:34:08 +0100

    Color histogram with distance added

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
 # This is a sample Python script.
+import glob
+import skimage
+import numpy as np
+from scipy.spatial import distance
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def data_base_histogram(folder_path):
+    colors = ("red", "green", "blue")
+    channel_ids = (0, 1, 2)
+    img_names = glob.glob(folder_path+'/*.jpg')
+    images_array = []
+    for img in img_names:
+        images_array.append(skimage.io.imread(img))
+
+    img_histograms = []
+    for image in images_array:
+        channel_histograms = []
+        for channel_id, c in zip(channel_ids, colors):
+            histogram, bin_edges = np.histogram(
+                image[:, :, channel_id], bins=256, range=(0, 256)
+            )
+            channel_histograms.append(histogram)
+
+        img_histograms.append(channel_histograms)
+
+    return img_names, img_histograms
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def compare_histograms(query_image, database_image, img_names):
+    img_distances = []
+    for img in database_image:
+        channel_distances = []
+        for img_channel in range(len(img)):
+            channel_distances.append(distance.euclidean(img[img_channel], query_image[img_channel]))
+        img_distances.append(np.mean(channel_distances)/sum(query_image[img_channel]))
+    result = sorted(zip(img_names, img_distances), key=lambda x: x[1])
+
+    return result
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+images_names, images_histograms = data_base_histogram("data")
+distances = compare_histograms(images_histograms[20], images_histograms, images_names)
+print(distances[:5])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+img = mpimg.imread(distances[0][0])
+imgplot = plt.imshow(img)
+plt.show()
+img = mpimg.imread(distances[1][0])
+imgplot = plt.imshow(img)
+plt.show()
+img = mpimg.imread(distances[10][0])
+imgplot = plt.imshow(img)
+plt.show()
+img = mpimg.imread(distances[-1][0])
+imgplot = plt.imshow(img)
+plt.show()
+
+
+
